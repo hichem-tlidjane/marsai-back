fc5daa2cdb648de8c11c8bd34cacfb362f1d1c89
johan <johan.chevet@laplateforme.io>
2026-02-16 09:19:45 +0100

    added plural to routes

--- src/server.ts
+++ src/server.ts
@@ -22,9 +22,9 @@ app.use(
 
 app.use('/auth', authRouter);
 app.use('/movies', movieRouter);
-app.use('/event', eventRouter);
-app.use('/newsletter', newsletterRouter);
-app.use('/subscriber', subscriberRouter);
+app.use('/events', eventRouter);
+app.use('/newsletters', newsletterRouter);
+app.use('/subscribers', subscriberRouter);
 
 app.use(errorHandler);
 
